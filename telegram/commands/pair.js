0646c9b1011f6ef6537db4581a35079481ac6ed3
Simon tech <officialsimondon@gmail.com>
2026-09-17 06:29:15 +0100

    Add pairCommand function for WhatsApp pairing

---
+++ telegram/commands/pair.js
@@ -0,0 +1,13 @@
+async function pairCommand(ctx) {
+    await ctx.reply(
+        "👑 MYSTERIOUS QUEEN\n\n" +
+        "📱 WHATSAPP PAIRING\n\n" +
+        "Send the WhatsApp phone number you want to pair.\n\n" +
+        "Example:\n" +
+        "2348012345678"
+    );
+}
+
+module.exports = {
+    pairCommand
+};
